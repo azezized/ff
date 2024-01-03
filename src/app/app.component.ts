b8bea9f961c07a626d0f27e8164213a65fe9c6f6
Aziz Diop <mouhamed-abdoul-aziz.diop@polymtl.ca>
2024-01-03 15:10:10 -0500

    changes

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'kirb';
+  photos = [
+    {
+      url: '../assets/fati5.JPG',
+      title:
+        "Il daut quand même avouer qu'elle est magnifique... Allahuma Bareek. Je pourrais faire une dissertation complète sur le seul critère de sa beauté, si bien que même qu'à la fin de la dissertaion, j'aurais épuisé les mots de la langue française, et j'aurais toujours des choses à dire. Ce qui frappe quand vous vous amusez à la regarder, c'est son sourire. C'est tellement intrigant que ça ne peut que vous troubler l'esprit, qu'importe la robustesse de celui-ci. Ensuite vient chaque détail de son visage, le tout formant un cocktail visuel indescriptible dans sa totalité. Son nez à moitié aquilin, ses yeux qui se plissent quand elle sourit, ses joues qui se creusent légèrement pour laisser paraitre un semblant de fossettes, ses lèvres qui se dessinent en un sourire parfait, ses dents qui se dévoilent... Si vous doutez encore de la création d'Allah, regardez-la et contemplez cette finesse. ",
+    },
+    {
+      url: '../assets/fati2.jpeg',
+      title:
+        "Je ne cesse de lui dire que ce qui la caractérise réellement, c'est son authenticité. Pour être tout à fait honnête avec vous, c'est ce que je préfère chez elle. Depuis ce 13 mai, jour de notre rencontre, découvrir les facettes de sa personnalité a été aussi enrichissant qu'amusant. Il est vrai que je vous ai précédemment dit qu'il m'arrive de manquer de mots lorsque je parle avec précision de sa beauté extérieure. Eh bien, Ô lecteurs, décrire sa personnalité est un exercice auquel je m'adonne souvent, et laissez-moi vous dire qu'à chaque essai, j'ai l'impression que je suis en train de la découvrir pour la première fois. C'est comme si elle était un livre dont je ne me lasserai jamais de lire. Son énergie vous engloutit et vous transporte dans un monde où vous ne pouvez que vous sentir bien. Sa naïveté est telle que l'on se demande si elle est réelle ou si elle est une façade. C'est à se demander si cette fille ne pense pas vivre dans un conte de fées. À plusieurs reprises, je me suis surpris en train de sourire bêtement en lui parlant, tellement j'étais à la fois amusé et charmé par cette naïveté, et je ne vous cache pas que cela m'arrive encore. Elle est tellement vraie que vous ne pouvez que l'aimer.",
+    },
+
+    {
+      url: '../assets/fati6.jpeg',
+      title:
+        " Quelques fois cette authenticité lui joue des tours, pour quelqu'un qui la connaît réellement, il lui est impossible de mentir, le ton de sa voix change automatiquement, le débit de parole est un peu plus lent que d'habitude et si vous êtes face à elle, il y a un élément qui la trahit. Un élément si expressif que vous ne pouvez que vous rendre compte qu'elle ment. Je m'abstiendrai de le décrire pour ne pas perdre cet avantage sur elle mdrr. Si vous lisez ce texte, c'est que vous la connaissez suffisamment pour pouvoir quantifier sa gentillesse. Rarement il m'a été donné de constater un tel degré de bonté chez une personne. Elle a le cœur sur la main Maa Shaa Allah, munie d'une empathie sans égale, elle est toujours là pour les autres. Son impatience constitue aussi un autre trait de caractère qui la définit. Elle serait prête à bouder parce que vous ne lui dites pas le cadeau que vous voulez lui offrir, ou encore l'endroit où vous voulez l'emmener. Elle veut tout savoir, jusqu'à la plus petite virgule, tout en détail, et le plus vite possible. Mais tâchez de résister face à cette pression qu'elle essaye d'imposer du haut de ses 1m60 pour que vous lui dévoilez ce qu'elle veut savoir. Elle est aussi très sensible, et c'est ce qui fait d'elle une personne très attachante, et personnellement je trouve que ce trait vous rend meilleur en tant que que communicateur. Car si vous tenez réellement à la personne d'en face, qu'elle soit sensible ou pas d'ailleurs, vous feriez plus attention à chaque mot qui sort de votre bouche, tant en situation normale qu'en temps de conflit. Pour terminer avec ce paragraphe, je dirait que c'est une personne qui vous fait découvrir une autre facette de vous-même, une facette que vous ne soupçonniez même pas. Elle vous fait découvrir des choses que vous ne pensiez pas pouvoir faire, et vous pousse à vous surpasser. Humainement, elle vous fait grandir. ",
+    },
+  ];
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainPageComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
