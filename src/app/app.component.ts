11f14382fbd181c160a3291846ef8b522d85cbd6
Aziz Diop <mouhamed-abdoul-aziz.diop@polymtl.ca>
2024-01-03 15:51:03 -0500

    modif

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,7 +22,50 @@ export class AppComponent {
     {
       url: '../assets/fati6.jpeg',
       title:
-        " Quelques fois cette authenticité lui joue des tours, pour quelqu'un qui la connaît réellement, il lui est impossible de mentir, le ton de sa voix change automatiquement, le débit de parole est un peu plus lent que d'habitude et si vous êtes face à elle, il y a un élément qui la trahit. Un élément si expressif que vous ne pouvez que vous rendre compte qu'elle ment. Je m'abstiendrai de le décrire pour ne pas perdre cet avantage sur elle mdrr. Si vous lisez ce texte, c'est que vous la connaissez suffisamment pour pouvoir quantifier sa gentillesse. Rarement il m'a été donné de constater un tel degré de bonté chez une personne. Elle a le cœur sur la main Maa Shaa Allah, munie d'une empathie sans égale, elle est toujours là pour les autres. Son impatience constitue aussi un autre trait de caractère qui la définit. Elle serait prête à bouder parce que vous ne lui dites pas le cadeau que vous voulez lui offrir, ou encore l'endroit où vous voulez l'emmener. Elle veut tout savoir, jusqu'à la plus petite virgule, tout en détail, et le plus vite possible. Mais tâchez de résister face à cette pression qu'elle essaye d'imposer du haut de ses 1m60 pour que vous lui dévoilez ce qu'elle veut savoir. Elle est aussi très sensible, et c'est ce qui fait d'elle une personne très attachante, et personnellement je trouve que ce trait vous rend meilleur en tant que que communicateur. Car si vous tenez réellement à la personne d'en face, qu'elle soit sensible ou pas d'ailleurs, vous feriez plus attention à chaque mot qui sort de votre bouche, tant en situation normale qu'en temps de conflit. Pour terminer avec ce paragraphe, je dirait que c'est une personne qui vous fait découvrir une autre facette de vous-même, une facette que vous ne soupçonniez même pas. Elle vous fait découvrir des choses que vous ne pensiez pas pouvoir faire, et vous pousse à vous surpasser. Humainement, elle vous fait grandir. ",
+        " Quelques fois cette authenticité lui joue des tours, pour quelqu'un qui la connaît réellement, il lui est impossible de mentir, le ton de sa voix change automatiquement, le débit de parole est un peu plus lent que d'habitude et si vous êtes face à elle, il y a un élément qui la trahit. Un élément si expressif que vous ne pouvez que vous rendre compte qu'elle ment. Je m'abstiendrai de le décrire pour ne pas perdre cet avantage sur elle mdrr. Si vous lisez ce texte, c'est que vous la connaissez suffisamment pour pouvoir quantifier sa gentillesse. Rarement il m'a été donné de constater un tel degré de bonté chez une personne. Elle a le cœur sur la main Maa Shaa Allah, munie d'une empathie sans égale, elle est toujours là pour les autres.",
+    },
+
+    {
+      url: '../assets/fati4.JPG',
+      title:
+        " Son impatience constitue aussi un autre trait de caractère qui la définit. Elle serait prête à bouder parce que vous ne lui dites pas le cadeau que vous voulez lui offrir, ou encore l'endroit où vous voulez l'emmener. Elle veut tout savoir, jusqu'à la plus petite virgule, tout en détail, et le plus vite possible. Mais tâchez de résister face à cette pression qu'elle essaye d'imposer du haut de ses 1m60 pour que vous lui dévoilez ce qu'elle veut savoir. Elle est aussi très sensible, et c'est ce qui fait d'elle une personne très attachante, et personnellement je trouve que ce trait vous rend meilleur en tant que que communicateur. Car si vous tenez réellement à la personne d'en face, qu'elle soit sensible ou pas d'ailleurs, vous feriez plus attention à chaque mot qui sort de votre bouche, tant en situation normale qu'en temps de conflit. Pour terminer avec ce paragraphe, je dirait que c'est une personne qui vous fait découvrir une autre facette de vous-même, une facette que vous ne soupçonniez même pas. Elle vous fait découvrir des choses que vous ne pensiez pas pouvoir faire, et vous pousse à vous surpasser. Humainement, elle vous fait grandir.",
+    },
+
+    {
+      url: '../assets/fati3.PNG',
+      title:
+        " Sa capacité à savourer chaque instant avec une sincérité déconcertante crée une ambiance où il est difficile de ne pas se laisser emporter par la gaieté qui l'habite. Elle ne se contente pas de vivre, elle célèbre la vie dans toute sa splendeur, et cette célébration devient une invitation pour les autres à partager cette joie intense. Vous savez mieux que moi que c'est une expérience unique que de partager des moments avec elle, car sa joie de vivre devient un catalyseur, transformant les moments ordinaires en souvenirs mémorables. Elle incarne la conviction que la vie mérite d'être célébrée à chaque instant. Sa joie de vivre est une lumière qui éclaire chaque instant partagé en sa compagnie. Depuis le jour où nos chemins se sont croisés, son enthousiasme contagieux a transformé ma perception du quotidien. C'est ainsi qu'un beau jour, je me suis surpris à réellement apprécier cette personne, à m'intéresser à elle et à ce qu'elle dégage. C'est comme si elle portait en elle un soleil intérieur qui irradie autour d'elle, créant une atmosphère chaleureuse.",
+    },
+
+    {
+      url: '../assets/IMG_1299.jpeg',
+      title:
+        "Je pense que je l'ai assez saucé là mdrr, il est temps de vous laissez la contempler, on commence avec une de mes photos préférées, je vous laisse deviner pourquoi.",
+    },
+
+    {
+      url: '../assets/IMG_1319.jpeg',
+      title: 'ooplaaaa !',
+    },
+
+    {
+      url: '../assets/IMG_1358.jpeg',
+      title: 'Elle regarde rien du tout',
+    },
+
+    {
+      url: '../assets/IMG_1358.jpeg',
+      title: 'Faudra nous dire après ce que tu regardais',
+    },
+
+    {
+      url: '../assets/fati7.jpeg',
+      title: 'Allahuma Bareek',
+    },
+
+    {
+      url: '../assets/IMG_7460.jpeg',
+      title: "Je vous laisse sur ce sourire à la limite de l'éblouissement",
     },
   ];
 }
